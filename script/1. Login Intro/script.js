8a39ba905ae355d6690d7173fdd639b6aef6928c
Legend-1409 <yashbansal1409@gmail.com>
2022-01-23 15:46:11 +0530

    added intro section

--- script/1. Login Intro/script.js
+++ script/1. Login Intro/script.js
@@ -2,8 +2,18 @@ let introText = document.querySelector('.login-background');
 let h1 = document.createElement("h1");
 let textNode = document.createTextNode("Portfolio");
 h1.appendChild(textNode);
+h1.classList.add('zoomIn');
+
+let loginIntroSection = document.querySelector("#login-intro-section");
+let introSection = document.querySelector('#intro-section');
+
 
 setTimeout(()=> {
     introText.removeChild(introText.firstElementChild);
     introText.appendChild(h1);
-}, 2300);
+}, 2500);
+
+setTimeout(()=> {
+    loginIntroSection.classList.add('invisible');
+    introSection.classList.remove('invisible');
+}, 5000);
