d33d1fa9d782a6d8566c9689cc556f6cfd4ca3f7
Legend-1409 <yashbansal1409@gmail.com>
2022-01-26 20:51:38 +0530

    fixed audio start

--- script/0. PreLoader Script/script.js
+++ script/0. PreLoader Script/script.js
@@ -1,5 +1,9 @@
 let pageLoader = document.querySelector('#preLoader');
+let loginIntroSection1 = document.querySelector('#login-intro-section');
+let musicSection = document.querySelector('#background-music');
 
 window.addEventListener('load', () => {
     pageLoader.classList.remove('is-active');
+    loginIntroSection1.classList.remove('invisible');
+    musicSection.play();
 });
